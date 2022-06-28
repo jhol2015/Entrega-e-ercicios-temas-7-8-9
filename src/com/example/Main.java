789c4cb10e226699d541b1d60712a39bc28b6d62
Jhol Moreira <galilleu2011@gmail.com>
2022-06-28 14:21:18 -0300

    Entrega ejercicios temas 7, 8 y 9 :construction:

---
+++ src/com/example/Main.java
@@ -0,0 +1,21 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //Sesion7.demoStrings();
+        //Sesion7.demoArrays();
+        //Sesion7.demoVector();
+        //Sesion7.demoList();
+        //Sesion7.demoMap();
+        //Sesion7.demoMath();
+
+        //Sesion8.errorDivisionPorCero();
+        //Sesion8.errorDivisionPorCeroTry();
+        //Sesion8.errorOutOfBounds();
+        //Sesion8.errorOutOfBoundsTry();
+        //Sesion8.errorLogico();
+
+        //Sesion9.leerByteAByte();
+        //Sesion9.leerTodo();
+    }
+}
